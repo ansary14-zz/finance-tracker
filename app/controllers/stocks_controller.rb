24ff5f03675f7ef2d9b5f241e239b40a952a00f8
ansary14 <ayubuapcse@gmail.com>
2020-11-13 00:47:28 +0600

    Dealing with invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "You must enter a valid symbol"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "You must enter a symbol"
+      redirect_to my_portfolio_path
+    end
   end
 
 end
